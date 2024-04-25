2e2f817e1d9bcde3f60a0726c9b8bfcbaf585d3c
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-04-26 00:45:45 +0530

    ⚒️chore: Removed unwanted routes.

--- server/index.js
+++ server/index.js
@@ -18,29 +18,6 @@ app.get('/',( req, res)=>{
    res.send('server is ok');
 })
 
-// Define a route to handle GET requests
-app.get('/api/data', (req, res) => {
-  try {
-    // Simulated data
-    const data = { message: 'This is the data from the server' };
-    
-    // Sending the data as a JSON response
-    res.json(data);
-  } catch (error) {
-    // If an error occurs, send an error response
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.post('/verify-otp',( req, res)=>{
-  const otp = req.body;
-  
-  // Print the OTP
-  console.log('Received OTP:',req.body);
-  res.status(200).send('OTP verification successful');
-})
-
-
 app.listen(8000, ()=>{
   console.log(`Server is runnig on port ${8000}`);
 })
