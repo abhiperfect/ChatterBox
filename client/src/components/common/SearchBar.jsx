e5389466774b469730d964e288ee3bd7feba1d8c
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-06-09 12:28:40 +0530

    🐛Fix/Fix search bar

--- client/src/components/common/SearchBar.jsx
+++ client/src/components/common/SearchBar.jsx
@@ -1,63 +1,55 @@
-import * as React from "react";
+import React, { useState, useRef } from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import { searchBarBGColor, searchBarTextColor, searchBarInsetBoxShadow } from "../../constants/color";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  searchBarBGColor,
+  searchBarTextColor,
+  searchBarInsetBoxShadow,
+} from "../../constants/color";
+import { useComponentContext } from "../../context/UserContext";
+import CloseIcon from "@mui/icons-material/Close";
+import { Stack } from "@mui/material";
+import { Input } from "@mui/material";
 
-
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
@@ -69,23 +61,19 @@ function SearchBar({
   borderTopRightRadius,
   borderBottomRightRadius,
 }) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const { searchBarOpen, setSearchBarOpen } = useComponentContext();
+  const [searchInput, setSearchInput] = useState("");
+  const searchBarRef = useRef(null);
 
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
+  const handleSearchClick = () => {
+    setSearchBarOpen(true);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const closeSearchBar = () => {
+    setSearchBarOpen(false);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value);
   };
 
   return (
@@ -96,29 +84,32 @@ function SearchBar({
         borderTopRightRadius: borderTopRightRadius,
         borderBottomLeftRadius: borderBottomLeftRadius,
         borderBottomRightRadius: borderBottomRightRadius,
-        backgroundColor:searchBarBGColor,
-        color:searchBarTextColor ,
-        height:'70px',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-
+        backgroundColor: searchBarBGColor,
+        color: searchBarTextColor,
+        height: "70px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      
     >
-      <Container maxWidth="xl">
-        <Search sx={{ boxShadow:`inset ${searchBarInsetBoxShadow} 0px 0px 15px -10px`
-        }}>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ "aria-label": "search" }}
-          />
-        </Search>
-      </Container>
+      <Search
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <SearchIcon />
+        <Input
+          placeholder="Find Friends..."
+          fullWidth={true}
+          onChange={handleInputChange}
+          onClick={handleSearchClick}
+        ></Input>
+        <CloseIcon onClick={closeSearchBar} sx={{ cursor:'pointer' }} />
+      </Search>
     </AppBar>
   );
 }
+
 export default SearchBar;
