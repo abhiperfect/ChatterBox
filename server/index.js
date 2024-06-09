280fc3896cb199221ea7ac0ebb356bcdcef778eb
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-06-09 15:06:16 +0530

    ⚒️Chore[Backend]/Clean up unwanted files.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser'; 
 import cors from 'cors';
-import authenticateRoute from './routes/authenticate.js'
-import connectToDatabase from './database/postgre.js';
-import messagesRouter from './controllers/messages.mjs';
-const db = connectToDatabase();
 const app = express();
 
 //%%MIDDLEWARE START %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', authenticateRoute);
-app.use('/api/messages', messagesRouter);
+
 //%%MIDDLEWARE END %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 app.get('/',( req, res)=>{
