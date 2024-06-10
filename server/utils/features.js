0b0cc369eef6d31c837116e1399d598c125040f8
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-06-10 23:05:57 +0530

    ⚒️Chore[Backend]/Commenting before making.

--- server/utils/features.js
+++ server/utils/features.js
@@ -34,9 +34,9 @@ const sendToken = (res, user, code, message) => {
   });
 };
 const emitEvent = (req, event, users, data) => {
-  const io = req.app.get("io");
-  const usersSocket = getSockets(users);
-  io.to(usersSocket).emit(event, data);
+  // const io = req.app.get("io");
+  // const usersSocket = getSockets(users);
+  // io.to(usersSocket).emit(event, data);
   console.log("Working emit event");
 };
 
