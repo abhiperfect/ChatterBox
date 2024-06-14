247c5c933217c3a072ef4af6aaab36a1946eae48
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-06-15 03:07:39 +0530

    ⚒️Chore[Frontend]/remove component fix it latter.

--- client/src/components/ui/icons/NewGroup.jsx
+++ client/src/components/ui/icons/NewGroup.jsx
@@ -4,12 +4,10 @@ import { IconButton } from "@mui/material";
 import NewGroupDialog from "../dialogs/NewGroupDialog";
 export default function NewGroupIcon({ badgeContent }) {
   return (
-    <NewGroupDialog>
       <Fragment>
         <IconButton>
           <GroupAddIcon sx={{ color: "white" }} />
         </IconButton>
       </Fragment>
-    </NewGroupDialog>
   );
 }
